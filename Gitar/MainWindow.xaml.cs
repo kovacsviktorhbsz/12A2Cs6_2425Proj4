9f9486ef0ef7ac1989ab4f4b431113342fbc0d46
bujtorbalinthbsz <bujtor.balint@hbsz.edu.hu>
2025-04-09 09:10:39 +0200

    Add files via upload

---
+++ Gitar/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gitar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = "server=localhost;port=3307;user=root;password=;database=gitar";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM zenekar";
+
+                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"ID: {reader["id"]}, Név: {reader["zenekarnev"]}");
+                    }
+                }
+            }
+ 
+            DataTable dt = new DataTable();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM zenekar", conn);
+                adapter.Fill(dt);
+                dataGrid.ItemsSource = dt.DefaultView;
+            }
+            //
+            //
+            //
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM muvesz";
+
+                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"ID: {reader["id"]}, Név: {reader["muvesznev"]}, Stílus: {reader["stilusid"]}, Nemzetiség:{reader["nemzetiseg"]}");
+                    }
+                }
+            }
+
+            DataTable dt2 = new DataTable();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM muvesz", conn);
+                adapter.Fill(dt2);
+                dataGrid2.ItemsSource = dt2.DefaultView;
+            }
+        }
+    }
+}
